d2d2b1f5755f3dbdb44c510171dbafb2b28da3ec
CuickCoder <120598559+CuickCoder@users.noreply.github.com>
2026-01-26 20:27:29 -0600

    Updated To Do List
    Updated to-do list with results from today's beta test, and implemented a button name change at Tafan's request

--- spec-ops-bomb-timer/TodoList.gd
+++ spec-ops-bomb-timer/TodoList.gd
@@ -1,6 +1,12 @@
 #TODO LIST 
 
-#change hard coded default panic mode time to 10 minutes
+#PANIC MODE NEEDS ATTENTION ASAP! The text wobble is hardly 
+#noticable on the smaller screen, and the time needs to be at
+#the 5 minute mark,10 minutes is too early it doesn't feel like
+#"crunch time" at the 10 minute mark. Look into adding a BB code
+#effect to the warning text as well. Need to make the shake range
+#much higher, keep the frequency, and maybe make the tape movement
+#faster or add spark particles during panic mode?
 
 #There's a value called "I" in the rgb modulate settings, 
 #the tint is barely noticable without it, need to figure out
@@ -12,7 +18,6 @@
 #Jillian didn't know if it was miniutes and seconds for the
 #custom time, said adding a colon between them would make
 #it clear what you're doing
-
 
 #Need to fix theme for the control screen 
 
